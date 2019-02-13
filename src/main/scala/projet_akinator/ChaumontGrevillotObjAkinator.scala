8a1e15bf3a650822cf06a60b5ae013bf0e49f741
Tristan CHAUMONT <tristan.chaumont68@gmail.com>
2019-02-13 08:28:44 +0100

    modif

--- src/main/scala/projet_akinator/ChaumontGrevillotObjAkinator.scala
+++ src/main/scala/projet_akinator/ChaumontGrevillotObjAkinator.scala
@@ -1,17 +1,17 @@
 package projet_akinator
 
-class objAnimal {
+object ChaumontGrevillotObjAkinator {
   
   trait ABanimal
-  case class Animal(nom:String) extends ABanimal
-  case class Question(q:String, oui:ABanimal, non:ABanimal) extends ABanimal
+  case class Animal(nom: String) extends ABanimal
+  case class Question(q: String, oui: ABanimal, non: ABanimal) extends ABanimal
  
   val a = Question("Est-ce qu'il a des ailes ?",
           Question("Est-ce qu'il a des plumes ?",
           Question("Est-ce qu'il a un goitre ?",
           Animal("Pélican"), Animal("Pigeon")),
           Question("Est-ce qu'il a des poils ?",
-          Animal("Chauve-souris"),Animal("Ptérodactyle"))),
+          Animal("Chauve-souris"), Animal("Ptérodactyle"))),
           Question("Est-ce qu'il ronronne ?",
           Animal("Chat"), Animal("Chien")))   
 }
