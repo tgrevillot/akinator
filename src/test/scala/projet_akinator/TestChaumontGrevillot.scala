d02bf49af3056a5d6309fce5e759286366a8f474
Tristan CHAUMONT <tristan.chaumont68@gmail.com>
2019-02-13 09:16:58 +0100

    tests premiere question

--- src/test/scala/projet_akinator/TestChaumontGrevillot.scala
+++ src/test/scala/projet_akinator/TestChaumontGrevillot.scala
@@ -1,5 +1,25 @@
 package projet_akinator
 
-class TestChaumontGrevillot {
+import org.scalatest._
+
+class TestChaumontGrevillot extends FunSuite {
   
+  import ChaumontGrevillotObjAkinator._
+  
+  val a = Question("Est-ce qu'il a des ailes ?",
+          Question("Est-ce qu'il a des plumes ?",
+          Question("Est-ce qu'il a un goitre ?",
+          Animal("Pélican"), Animal("Pigeon")),
+          Question("Est-ce qu'il a des poils ?",
+          Animal("Chauve-souris"), Animal("Ptérodactyle"))),
+          Question("Est-ce qu'il ronronne ?",
+          Animal("Chat"), Animal("Chien")));
+  
+  test("jeuSimple bonne reponse") {
+    assert(jeuSimple(a, Iterator("o", "o", "o", "o")) === true)
+  }
+  
+  test("jeuSimple mauvaise reponse") {
+    assert(jeuSimple(a, Iterator("n", "n", "n")) === false)
+  }
 }
