c0b4ae36b5f51660bdf2e4eb2fb6334a6973e8e2
Tristan CHAUMONT <tristan.chaumont68@gmail.com>
2019-02-13 09:12:28 +0100

    premiere question

--- src/main/scala/projet_akinator/ChaumontGrevillotObjAkinator.scala
+++ src/main/scala/projet_akinator/ChaumontGrevillotObjAkinator.scala
@@ -1,5 +1,8 @@
 package projet_akinator
 
+import scala.io._
+import java.io._
+
 object ChaumontGrevillotObjAkinator {
   
   trait ABanimal
@@ -15,7 +18,30 @@ object ChaumontGrevillotObjAkinator {
           Question("Est-ce qu'il ronronne ?",
           Animal("Chat"), Animal("Chien")));
           
-  /* Question 1 */
+  /* Question 1 / 2 */
   
+  def jeuSimple(a: ABanimal, it: Iterator[String]): Boolean = a match {
+    case Question(q, oui, non) => {
+      println(q)
+      if(it.next() == "o") {
+        jeuSimple(oui, it)
+      } else {
+        jeuSimple(non, it)
+      }
+    }
+    case Animal(nom) => {
+      println("Pensez-vous à : " + nom + " ?")
+      if(it.next() == "o") {
+        println("J'ai gagné")
+        true
+      } else {
+        println("J'ai perdu")
+        false
+      }
+    }
+  }
   
+  def main(args: Array[String]): Unit = {
+    jeuSimple(a, Source.stdin.getLines)
+  }
 }
